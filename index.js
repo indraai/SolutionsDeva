bdb54714f936fae5180fc8c41d1145833c431794
Quinn <quinn@indra.ai>
2023-07-08 13:30:35 -0700

    update solutions

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const info = {
   describe: package.description,
   version: package.version,
   url: package.homepage,
+  dir: __dirname,
   git: package.repository.url,
   bugs: package.bugs.url,
   author: package.author,
@@ -44,55 +45,10 @@ const SOLUTIONS = new Deva({
   modules: {},
   deva: {},
   func: {
-    sol_question(packet) {
-      const agent = this.agent();
-      const solutions = this.solutions();
-      solutions.personal.questions.push(packet);
-    },
-    sol_answer(packet) {
-      const agent = this.agent();
-      const solutions = this.solutions();
-      solutions.personal.answers.push(packet);
-    },
+    sol_question(packet) {return;},
+    sol_answer(packet) {return;},
   },
-  methods: {
-    /**************
-    method: uid
-    params: packet
-    describe: Return a system id to the user from the :name:.
-    ***************/
-    uid(packet) {
-      return Promise.resolve({text:this.uid()});
-    },
-
-    /**************
-    method: status
-    params: packet
-    describe: Return the current status of the :name:.
-    ***************/
-    status(packet) {
-      return this.status();
-    },
-
-    /**************
-    method: help
-    params: packet
-    describe: The Help method returns the information on how to use the :name:.
-    ***************/
-    help(packet) {
-      return new Promise((resolve, reject) => {
-        this.lib.help(packet.q.text, __dirname).then(help => {
-          return this.question(`#feecting parse ${help}`);
-        }).then(parsed => {
-          return resolve({
-            text: parsed.a.text,
-            html: parsed.a.html,
-            data: parsed.a.data,
-          });
-        }).catch(reject);
-      });
-    }
-  },
+  methods: {},
   onDone(data) {
     this.listen('devacore:question', packet => {
       if (packet.q.text.includes(this.vars.trigger)) return this.func.sol_question(packet);
